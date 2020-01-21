b5f6d73b21ec8886eaad07278d5aa4b2871a5dc6
ngumina17 <nicole.gumina17@gmail.com>
2020-01-21 12:06:44 -0600

    successful post

--- controllers/parisController.js
+++ controllers/parisController.js
@@ -7,4 +7,27 @@ router.get("/", (req, res) => {
     Paris.find({}).then(ideas => res.json(ideas));
 })
 
+router.post("/", (req, res) => {
+    let newIdea = req.body;
+    // console.log('this is new Gif', newGif);
+    Paris.create(newIdea).then(Paris.find({}).then(ideas => res.json(ideas))
+    // res.json( {newIdea} )
+)})
+
+// router.put("/:id", (req, res) => {
+//     console.log(req.params.id)
+//     let giffy = req.body
+//     let id = req.params.id
+//     Gifs.findOneAndUpdate({ _id: id }, giffy, { new: true })
+//       .then(() => {Gifs.find({}).then(gifs => res.json(gifs))
+//     });
+// })
+
+// router.delete('/:id', (req, res) => {
+//     Gifs.findOneAndRemove({ _id: req.params.id })
+//       .then(() => {
+//         res.redirect('/')
+//       })
+//   })
+
 module.exports = router
