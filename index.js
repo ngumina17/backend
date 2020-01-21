2f0a6a2867cc6495adb4fcd9bde497edc32019b8
ngumina17 <nicole.gumina17@gmail.com>
2020-01-21 15:16:04 -0600

    installed cors

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express()
 const parser = require('body-parser')
 const parisController = require("./controllers/parisController")
+var cors = require('cors')
 
 
 
 app.use(parser.urlencoded({ extended: true }))
 app.use(parser.json())
+app.use(cors())
 
 app.get('/', (req,res) => {
     res.send('hitting default route')
@@ -19,4 +21,4 @@ app.use("/paris/", parisController)
 
 
 
-app.listen(3000, () => console.log("Running on port 3000!"))
+app.listen(4000, () => console.log("Running on port 3000!"))
