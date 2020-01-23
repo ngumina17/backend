786e999540098ed7996a091b1832e41918118770
ngumina17 <nicole.gumina17@gmail.com>
2020-01-23 15:12:50 -0600

    get, post, delete works for testing

---
+++ test/paris.test.js
@@ -0,0 +1,93 @@
+const should = require('chai').should
+const expect = require('chai').expect
+const supertest = require('supertest')
+const api = supertest('http://localhost:4000')
+
+describe("GET /paris", function () {
+    it('should return a 200 response', function (done) {
+        api
+        .get("/Paris")
+        .set("Accept", "applicaton/json")
+        .expect(200, done)
+    });
+
+    it('should return an array', function(done) {
+        api.get('/Paris')
+        .set('Accept', 'application/json')
+        .end(function(error, response) {
+            expect(response.body).to.be.an('array')
+            done()
+        })
+    })
+    it("should return an array of objects that have a field called 'idea' ", function(done){
+        api
+          .get("/Paris")
+          .set("Accept", "application/json")
+          .end(function(error, response){
+            expect(response.body[0]).to.have.property('idea');
+            done()
+        })
+      })
+    })
+
+    // describe('POST /Paris', function () {
+    //     before(function(done){
+    //         api.post('/Paris')
+    //         .set('Accept', 'application/json')
+    //         .send({
+    //             "idea": "Visit Catacombs",
+    //             "favorited": false,
+    //         })
+    //         .end(done)
+    //     })
+        
+    
+    //     it("should add a idea object to the collection Paris and return it", function (done) {
+    //         api
+    //           .get("/Paris")
+    //           .set("Accept", "application/json")
+    //           .end(function(error, response){
+    //             expect(response.body.length).to.equal(7);
+    //             done()
+    //           })
+    //       })
+    // })
+
+
+    describe('DELETE /Paris/:id',  () => {
+      
+        let previousLength
+        let idToDelete
+      
+        before( done => {
+          api
+            .get('/Paris')       
+            .set('Accept', 'application/json')       
+            .end( (error, response) =>  {
+              previousLength = response.body.length       
+              idToDelete = response.body[0]._id     
+              done()       
+            })
+        })
+      
+        before(done => {
+            console.log(idToDelete)
+          api.delete(`/Paris/${idToDelete}`)
+             .set('Accept', 'application/json')
+             .end( (error, response) => {
+               done()
+             })
+        })
+      
+        it('deletes an idea by id',  done => {
+          api.get('/Paris')
+             .set('Accept', 'application/json')
+             .end( (error, response) => {
+               expect(response.body.length).to.equal(previousLength - 1)
+               expect(response.body.find((ideas) => ideas.id == idToDelete)).to.equal(undefined)
+               done()
+             })
+        })
+      })
+      
+     
